540b8c13eebc8a9a1271ad5fefccd0c5fd34ef30
Dashbloxx <dashbloxx@gmail.com>
2023-04-26 15:16:09 -0700

    Project: ACTUALLY rebuild if configuration changes...

--- common/configuration.h
+++ common/configuration.h
@@ -1,8 +1,4 @@
-#pragma once
-
-#include <common/configuration_defines.h>
-
-/*
- *  In the future, some preprocessor scripts may be added here to check if the configuration is valid, but for now I won't
- *  implement them...
- */
+#define BOOTLOADER "GRUB"
+#define ARCHITECTURE "i?86"
+#define FORM "PC"
+#define FONT "/usr/share/fonts/zap-vga16.psf"
--- common/configuration_defines.h
+++
@@ -1,4 +0,0 @@
-#define BOOTLOADER "GRUB"
-#define ARCHITECTURE "i?86"
-#define FORM "PC"
-#define FONT "/usr/share/fonts/zap-vga16.psf"
